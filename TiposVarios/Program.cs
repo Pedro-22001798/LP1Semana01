3a0e12ece994cfad22b0178eb44bed66de9afa46
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-10 12:04:01 +0000

    Moving exercises

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            sbyte one = -128;
+            short two = -32768;
+            int three = 50000;
+            long four = 4611686018427387904;
+            byte five = 254;
+            ushort six = 65534;
+            uint seven = 4294967295;
+            ulong eight = 18446744073709551615;
+
+            Console.WriteLine(one);
+            Console.WriteLine(two);
+            Console.WriteLine(three);
+            Console.WriteLine(four);
+            Console.WriteLine(five);
+            Console.WriteLine(six);
+            Console.WriteLine(seven);
+            Console.WriteLine(eight);
+            
+            char nine = '\u00A9';
+            char ten = '\u225B';
+            char eleven = '\u2473';
+
+            Console.WriteLine(nine);
+            Console.WriteLine(ten);
+            Console.WriteLine(eleven);
+
+            float floatOne = 5.753333333333F;
+            double doubleOne = 19.999999999999999999D;
+            decimal decimalOne = 0.5555555555555555555555M;
+
+            Console.WriteLine(floatOne);
+            Console.WriteLine(doubleOne);
+            Console.WriteLine(decimalOne);
+
+            bool boolOne = true;
+            bool boolTwo = false;
+
+            Console.WriteLine(boolOne);
+            Console.WriteLine(boolTwo);
+        }
+    }
---
+++ VariasStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] stringNums)
+        {
+            string one = "simples";
+            string two = "\n";
+            string three = @"Não é um codigo Unicode \u1234, não é' nova linha \n";
+            string four = @"""Esta string esta entre aspas""";
+            string five = "Esta string não está entre aspas";
+
+            Console.WriteLine(two);
+            Console.WriteLine(one);
+            Console.WriteLine(three);
+            Console.WriteLine(four);
+            Console.WriteLine(five);
+
+            string six = "a" + 2;
+            int x = 500;
+            int y = 100;
+            string seven = $"valor de x é {x}";
+            string eight = $"valor de x + y = {x+y}";
+            string nine = String.Format("Y é {1}, X é {0}", x,y);
+
+            Console.WriteLine(six);
+            Console.WriteLine(seven);
+            Console.WriteLine(eight);
+            Console.WriteLine(nine);
+            Console.WriteLine(@"Verbatim com x = {0}",x);
+        }
+    }
